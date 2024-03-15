6b441c2860e5b1e2bab81e3a5c6d356bb194b73c
Foysalrabby <81823837+Foysalrabby@users.noreply.github.com>
2024-03-15 21:58:26 +0600

    done click to show string

--- lib/priceandlinkumage.dart
+++ lib/priceandlinkumage.dart
@@ -6,6 +6,10 @@ class Pricerange extends StatefulWidget{
   State<StatefulWidget> createState() => Implepricerange();
 }
 class Implepricerange extends State<Pricerange>{
+  static int indexi=0;
+  List quote=[
+    "a","b","c","d","e","f","g","h","i",
+  ];
   @override
   Widget build(BuildContext context) {
   return Scaffold(
@@ -13,12 +17,42 @@ class Implepricerange extends State<Pricerange>{
       title: Text("Price range"),
       backgroundColor: Colors.amber,
     ),
-    body: Container(
-      color: Colors.cyan,
-      child: Text("hi"),
+    body: Center(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+            children: [
+              Container(
+                width: 300.0,
+                height: 300.0,
+                child: Center(child: Text(quote[indexi%quote.length]), //to use % to get 1 postion or index
+                ),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10.0),
+                  color: Colors.deepOrangeAccent,
+                ),
+              ),
+             SizedBox(
+               height: 10.0,
+             ),
+
+             MaterialButton(onPressed: qouteget,
+
+               child: Text("Click to"),
+               color: Colors.yellow,
+             )
+            ],
+        ),
+      ),
     ),
   );
   }
 
 
+
+  void qouteget() {
+    setState(() {
+      indexi += 1;
+    });
+  }
 }
