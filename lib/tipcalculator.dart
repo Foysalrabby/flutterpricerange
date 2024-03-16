5386a2d265a8880350fe8770a7ae8ce79800e9e2
Foysalrabby <81823837+Foysalrabby@users.noreply.github.com>
2024-03-17 01:08:06 +0600

    margin with height and list tem

--- lib/tipcalculator.dart
+++ lib/tipcalculator.dart
@@ -15,7 +15,30 @@ class ExtendTipcalculator extends State<Tipcalculator>{
       ),
       body: Container(
         alignment: Alignment.center,
+        margin: EdgeInsets.only(top: MediaQuery.of(context).size.height*0.1), //to height noujayi set hobe
         color: Colors.white,
+        child: ListView(
+          scrollDirection: Axis.vertical,
+          padding: EdgeInsets.all(10),
+          children: [
+             Container(
+               width: 300,
+               height: 160,
+               decoration: BoxDecoration(
+                 borderRadius: BorderRadius.all(Radius.circular(20)),
+                 color: Colors.purpleAccent.shade100,
+               ),
+               child: Column(
+                 mainAxisAlignment: MainAxisAlignment.center,
+                 children: [
+                   Text("Total person", style: TextStyle(color: Colors.white, fontSize: 30,fontWeight: FontWeight.w700),),
+                   Text("\$123",style: TextStyle(color: Colors.white, fontSize: 30,fontWeight: FontWeight.w700),)
+                 ],
+               ),
+             ),
+            
+          ],
+        ),
       ),
     );
 
