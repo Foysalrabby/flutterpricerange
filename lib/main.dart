085b6627ff4b4dae86b923ba16a37b608a6026fd
Foysalrabby <81823837+Foysalrabby@users.noreply.github.com>
2024-03-19 00:05:29 +0600

    to remove space in top use spacer

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutterpricerange/priceandlinkumage.dart';
+import 'package:flutterpricerange/quize.dart';
 import 'package:flutterpricerange/tipcalculator.dart';
 
 void main() => runApp( new  MaterialApp(
-  home: Tipcalculator(),
+  home: Quizeapp()
 ));
 
 
--- lib/quize.dart
+++ lib/quize.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -9,6 +11,17 @@ class Quizeapp extends StatefulWidget{
 
 }
 class Extendedquize extends State<Quizeapp>{
+
+  List quizebank=[
+    "The capital of Bangladesh is Dhaka",
+    "The national flower of Bangladesh is the rose.",
+    "The Sundarbans, located in Bangladesh, is the largest mangrove forest in the world",
+    "Bengali (Bangla) is the only official language of Bangladesh",
+    "Bangladesh shares its longest border with  mayenmar",
+    "The traditional musical instrument tabla is commonly used in Bangladeshi music",
+    "Bangladesh was formerly known as East Pakistan before gaining independence",
+    "The official religion of Bangladesh is Hinduism"
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,12 +30,22 @@ class Extendedquize extends State<Quizeapp>{
         backgroundColor: Color.fromARGB(255, 223, 142, 61),
       ),
       body: Container(
+       width:double.infinity ,
         color:Color.fromARGB(255, 53, 108, 107) ,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
 
-          ],
+            children: [
+               Image.asset("assets/images/hockeypic.jpg"),
+              SizedBox(height: 10,),
+              Container(
+                child: Text("${quizebank[4]}",style: TextStyle(color: Colors.white, fontWeight: FontWeight.w300,fontSize: 20),),
+              ),
+              Spacer(), //to remove space in top
+            ],
+          ),
         ),
       ),
     );
