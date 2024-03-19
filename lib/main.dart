63e552065180a7994c760290205ee9a89d3b2ac8
Foysalrabby <81823837+Foysalrabby@users.noreply.github.com>
2024-03-20 00:57:56 +0600

    AGAIN LISTVIEW.BULDER PRACTICE FOR MOVIE

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutterpricerange/priceandlinkumage.dart';
-import 'package:flutterpricerange/quize.dart';
-import 'package:flutterpricerange/tipcalculator.dart';
+import 'package:flutterpricerange/movie.dart';
+// import 'package:flutterpricerange/priceandlinkumage.dart';
+// import 'package:flutterpricerange/quize.dart';
+// import 'package:flutterpricerange/tipcalculator.dart';
 
 void main() => runApp( new  MaterialApp(
-  home: Quizeapp()
+  home: Movieclass()
 ));
 
 
---
+++ lib/movie.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Movieclass extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    return ExtendMovieclss();
+  }
+
+}
+class ExtendMovieclss extends State<Movieclass>{
+  List moviename=[
+    " A Space Odyssey hero",
+    "The Godfather",
+    "Citizen Kane",
+    "Jeanne Dielman",
+    "Raiders of",
+    "La Dolce Vita",
+    "Seven Samurai",
+    "In the Mood love"
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Movie home"),
+        backgroundColor: Colors.blueGrey,
+      ),
+      backgroundColor: Colors.deepOrangeAccent,
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: ListView.builder(
+          itemCount: moviename.length,
+            itemBuilder: (BuildContext context,int index){
+              return  Card(
+                   child: ListTile(
+                     leading: CircleAvatar(
+                       child: Text("H"),
+                     ),
+                     title: Text("${moviename.elementAt(index)}"),
+                   ),
+              );
+            }
+        ),
+      ),
+    );
+
+  }
+
+}
