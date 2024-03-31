7b5e4a3ffd4a438099d532bbcf731ab6d1b06ebc
Foysalrabby <81823837+Foysalrabby@users.noreply.github.com>
2024-04-01 00:24:10 +0600

    start json parsing problem hhtp vesrion searcn to chatgpt

---
+++ lib/jsonparsing/jsonparsing.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+
+class Jsonparsing extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    throw UnimplementedError();
+  }
+
+}
+class ExtendJsonparding extends  State<Jsonparsing>{
+  @override
+  Widget build(BuildContext context) {
+   return Container();
+  }
+
+
+}
