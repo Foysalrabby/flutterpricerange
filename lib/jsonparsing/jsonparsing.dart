d934e98270195729df9381ec2beda4d7a859ca13
Foysalrabby <81823837+Foysalrabby@users.noreply.github.com>
2024-04-01 01:42:30 +0600

    this is right code

--- lib/jsonparsing/jsonparsing.dart
+++ lib/jsonparsing/jsonparsing.dart
@@ -1,18 +1,44 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class Jsonparsing extends StatefulWidget{
   @override
   State<StatefulWidget> createState() {
-    // TODO: implement createState
-    throw UnimplementedError();
+    return ExtendJsonparding();
   }
 
 }
 class ExtendJsonparding extends  State<Jsonparsing>{
+  
   @override
   Widget build(BuildContext context) {
-   return Container();
+   return Scaffold(
+     appBar: AppBar(
+       title: Text("Json Parsing",),
+       backgroundColor:Colors.deepOrangeAccent
+     ),
+   );
   }
 
+}
+class Networkdata{
+  final String url;
+  Networkdata(this.url);
 
+  Future fetchData() async{
+    print("$url");
+    //Response response=await Uri get(Uri.encodeFull(url));
+    http.Response response= await http.get(Uri.parse(url));
+    if(response.statusCode == 200){
+      print(response.body);
+      return response.body;
+      
+    }
+       else{
+
+         print(response.statusCode);
+    }
+
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:flutterpricerange/movie.dart';
+import 'package:flutterpricerange/jsonparsing/jsonparsing.dart';
+//import 'package:flutterpricerange/movie.dart';
 // import 'package:flutterpricerange/priceandlinkumage.dart';
 // import 'package:flutterpricerange/quize.dart';
 // import 'package:flutterpricerange/tipcalculator.dart';
 
 void main() => runApp( new  MaterialApp(
-  home: Movieclass()
+  home: Jsonparsing()
 ));
 
 
